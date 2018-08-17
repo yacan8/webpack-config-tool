879a0cdfaae20870b25a316a2c03143329e0c5bd
yacan8 <764271109@qq.com>
2018-08-17 17:56:02 +0800

    clean-webpack-plugin

--- dll/webpack.dll.reference.config.js
+++ dll/webpack.dll.reference.config.js
@@ -12,11 +12,11 @@ const plugins = [
   new CopyWebpackPlugin([{ from: path.join(process.cwd(), isDebug ? './vendor-dev/' : './vendor/'), to: baseConfig.output.path, ignore: ['*.json']}]) // 将dll文件拷贝到编译目录
 ];
 
-Object.keys(dllEntry).forEach((key) => {
+Object.keys(dllEntry).forEach(function(key) {
   const manifest = path.join(dllPath, `${key}.js.json`);
   plugins.push(new webpack.DllReferencePlugin({
     manifest: require(manifest), // 引进dllPlugin编译的json文件
-    name: `${key}_library` // 全局变量名，与dllPlugin声明的一直
+    name: key + '_library' // 全局变量名，与dllPlugin声明的一直
   }))
 })
 
